96d3328c412e7d2e7dfe7a30591e25282aab3e11
Ayush Nair <ayushnair.noida@gmail.com>
2024-08-11 18:31:19 -0700

    PEG-1: added vitest

---
+++ app/page.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { getCurrentFolderNameFromPath } from "../utils";
+import Home from "./page";
+
+describe(getCurrentFolderNameFromPath(__dirname), () => {
+  it("render content", () => {
+    render(<Home />);
+
+    expect(
+      screen.getByRole("heading", { level: 1, name: "Home" })
+    ).toBeDefined();
+  });
+});
