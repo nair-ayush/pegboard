66b8e755805f61f500cc89dfc9fd4e0f3fc096dd
Ayush Nair <39295763+nair-ayush@users.noreply.github.com>
2024-08-11 20:15:08 -0700

    PEG-11: checking production on vercel (#16)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 import Link from "next/link";
 
 export default function Page() {
@@ -5,6 +9,8 @@ export default function Page() {
     <div>
       <h1>Home</h1>
       <Link href="/about">About</Link>
+      <LoginLink>Sign in</LoginLink>
+      <RegisterLink>Sign up</RegisterLink>
     </div>
   );
 }
