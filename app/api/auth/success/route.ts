f854fd32c0dca59b1cdc97154539b14665df5a0d
Ayush Nair <39295763+nair-ayush@users.noreply.github.com>
2024-08-11 22:10:53 -0700

    PEG-12: added prisma and connected to auth with expenses model (#17)
    * PEG-12: added prisma and connected to auth with expenses model
    
    * PEG-12: updating package.json for latest prisma client fix
    
    * PEG-12: maybe fix
    
    * PEG-12: another fix
    
    * PEG-12: final fix

---
+++ app/api/auth/success/route.ts
@@ -0,0 +1,45 @@
+import db from "@/utils/db";
+import { getBaseUrl } from "@/utils/url";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || user == null || !user.id)
+    throw new Error("something went wrong with authentication");
+
+  let dbUser = await db.user.findUnique({
+    where: { authId: user.id },
+  });
+
+  if (!dbUser) {
+    dbUser = await db.user.create({
+      data: {
+        authId: user.id,
+        givenName: user.given_name ?? "",
+        familyName: user.family_name ?? "",
+        email: user.email ?? "",
+        paymentMethods: {
+          create: [
+            { name: "Cash" },
+            { name: "Credit Card" },
+            { name: "Account Transfer" },
+          ],
+        },
+        recurringIntervals: {
+          create: [
+            { interval: "WEEKLY" },
+            { interval: "MONTHLY" },
+            { interval: "ANNUALLY" },
+          ],
+        },
+      },
+    });
+  }
+
+  return NextResponse.redirect(`${getBaseUrl()}/dashboard`);
+}
